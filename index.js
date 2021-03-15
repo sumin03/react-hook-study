85ea91e013ad94a9863ea63570ff33eeadabb1d6
sumin03 <breath.ing0210@gmail.com>
2021-03-15 22:16:23 +0900

    study useEffect

--- index.js
+++ index.js
@@ -38,3 +38,29 @@ const App = () => {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const useTitle = (intialTitle) => {
+  const [title, setTitle] = useState(intialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
